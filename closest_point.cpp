f0437ee19e726c37f2f8738e405f0d141b4522b8
khaled hshmt <khaled.hshmt@marses.systems>
2021-05-03 15:00:50 +0200

    spliiting the code into .cpp and .hpp files and adding .gitignore file

--- closest_point.cpp
+++ closest_point.cpp
@@ -14,121 +14,8 @@
 -------------------------
 **find the closest points of O's to x position**
 */
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <math.h>
-#include <map>
-#include <fstream>
-#include <memory>
-#include <numeric>
+#include "kdtree.hpp"
 
-typedef unsigned int unit;
-
-class Point
-{
-    public:
-        Point(float x_=0, float y_=0) : x{x_}, y{y_}{} 
-
-        friend float eculdianDistance(Point& p1, Point& p2);
-        
-        float  get(int dim)
-        {
-            if(dim == 0)
-                return this->x;
-            else
-                return this->y;
-        }
-  
-        void printPoint()
-        {
-            std::cout << "("<<this->x <<", "<<this->y <<")" << std::endl;
-        }
-    private:
-        float x, y;
-};
-
-float eculdianDistance(Point& p1, Point& p2)
-{
-    float x_diff_sq = pow((p2.x-p1.x), 2);
-    float y_diff_sq = pow((p2.y-p1.y), 2);
-    return std::sqrt(x_diff_sq+y_diff_sq);
-}
-
-/*===========================================*/
-struct Node
-{
-	Point point;
-	int id;
-	Node* left;
-	Node* right;
-
-	Node(Point p, int setId) : point(p), id(setId), left(NULL), right(NULL)
-	{}
-};
-
-
-struct KdTree
-{
-	Node* root;
-	KdTree():root(NULL)
-	{}
-/*=========================================Insert method==================================================================
-Insert method used to insert point in the KDtree architecture 
-*/
-	void insertHelper(Node** node, unit depth, Point point, int id)
-	{
-		if(*node == NULL)
-			*node = new Node(point, id);
-		else
-		{
-			unit cd = depth % 2;
-			
-			if(point.get(cd) < (*node)->point.get(cd))
-			       insertHelper(&((*node)->left), depth+1, point, id);
-			else	
-			       insertHelper(&((*node)->right), depth+1, point, id);
-		}
-	}
-
-	void insert(Point point, int id)
-	{
-		insertHelper(&root, 0, point, id);	
-	}
-/*==================================================search methods======================================================
-Search method used to search allover the tree for points which is the closest to a given point according
-to a specific distance tolerance
-*/
-	void searchHelper(Point target, Node* node, int depth, std::vector<int>& ids, std::map<float, Point>& dis_point)
-	{
-        float min_distance = std::numeric_limits<double>::max();
-		if(node != NULL)
-		{
-
-			float distance = eculdianDistance(node->point, target);
-			if(distance < min_distance)
-            {
-				dis_point.emplace(std::make_pair(distance, node->point));
-                ids.push_back(node->id);
-            }
-
-			if((target.get(depth%2)) < node->point.get(depth%2))
-				searchHelper(target, node->left, depth+1, ids, dis_point);
-			if((target.get(depth%2)) > node->point.get(depth%2))
-				searchHelper(target, node->right, depth+1, ids, dis_point);
-		}
-	}
-
-	std::map<float, Point> search(Point target)
-	{
-		std::vector<int> ids;
-        std::map<float, Point> dis_point;
-		searchHelper(target, root, 0, ids, dis_point);
-		return dis_point;
-	}
-	
-
-};
 /*==========================MAIN FUNCTION================================*/
 int main()
 {
@@ -139,6 +26,8 @@ int main()
     std::cin >> Y;
 
     Point p(X,Y);
+    std::cout << "your point is ==> ";
+    p.printPoint();
     
     std::vector<Point> points;
 
@@ -171,10 +60,10 @@ int main()
     }
 
     std::map<float, Point> dis_point = tree->search(p);
- 
+    
+    std::cout << "the closest point to your point is ==> ";
     dis_point.begin()->second.printPoint();
-    std::cout << dis_point.begin()->first << std::endl;
+    std::cout << "the distance from your point to the closest point is ==> "<< dis_point.begin()->first << std::endl;
  
-
     return 0;
 }
---
+++ kdtree.hpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <map>
+#include <fstream>
+#include <memory>
+#include <numeric>
+
+typedef unsigned int unit;
+
+class Point
+{
+    public:
+        Point(float x_=0, float y_=0) : x{x_}, y{y_}{} 
+
+        friend float eculdianDistance(Point& p1, Point& p2);
+        
+        float  get(int dim)
+        {
+            if(dim == 0)
+                return this->x;
+            else
+                return this->y;
+        }
+  
+        void printPoint()
+        {
+            std::cout << "("<<this->x <<", "<<this->y <<")" << std::endl;
+        }
+    private:
+        float x, y;
+};
+
+float eculdianDistance(Point& p1, Point& p2)
+{
+    float x_diff_sq = pow((p2.x-p1.x), 2);
+    float y_diff_sq = pow((p2.y-p1.y), 2);
+    return std::sqrt(x_diff_sq+y_diff_sq);
+}
+
+/*==================================================================================*/
+struct Node
+{
+    public:
+        Point point;
+        int id;
+        Node* left;
+        Node* right;
+
+        Node(Point p, int setId) : point(p), id(setId), left(NULL), right(NULL)
+        {}
+};
+
+
+struct KdTree
+{
+    public:
+        Node* root;
+        KdTree():root(NULL)
+        {}
+/*=========================================Insert method=======================================
+Insert method used to insert point in the KDtree architecture 
+*/
+        void insertHelper(Node** node, unit depth, Point point, int id)
+        {
+            if(*node == NULL)
+                *node = new Node(point, id);
+            else
+            {
+                unit cd = depth % 2;
+                
+                if(point.get(cd) < (*node)->point.get(cd))
+                    insertHelper(&((*node)->left), depth+1, point, id);
+                else	
+                    insertHelper(&((*node)->right), depth+1, point, id);
+            }
+        }
+
+        void insert(Point point, int id)
+        {
+            insertHelper(&root, 0, point, id);	
+        }
+/*==================================================search methods================================
+Search method used to search allover the tree for points which is the closest to a given point according
+to a specific distance tolerance
+*/
+        void searchHelper(Point target, Node* node, int depth, std::vector<int>& ids, std::map<float, Point>& dis_point)
+        {
+            float min_distance = std::numeric_limits<double>::max();
+            if(node != NULL)
+            {
+
+                float distance = eculdianDistance(node->point, target);
+                if(distance < min_distance)
+                {
+                    dis_point.emplace(std::make_pair(distance, node->point));
+                    ids.push_back(node->id);
+                }
+
+                if((target.get(depth%2)) < node->point.get(depth%2))
+                    searchHelper(target, node->left, depth+1, ids, dis_point);
+                if((target.get(depth%2)) > node->point.get(depth%2))
+                    searchHelper(target, node->right, depth+1, ids, dis_point);
+            }
+        }
+
+        std::map<float, Point> search(Point target)
+        {
+            std::vector<int> ids;
+            std::map<float, Point> dis_point;
+            searchHelper(target, root, 0, ids, dis_point);
+            return dis_point;
+        }
+	
+
+};
